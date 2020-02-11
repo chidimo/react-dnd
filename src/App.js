9f29910bb7992e5a9adb53d6b2228f0a49ba8e37
Chidi Orji <orjichidi95@gmail.com>
2020-02-11 11:27:38 +0100

    feat: define drop zone

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import DragAndDrop from './DragAndDrop'
 
 function App() {
   return (
     <div className="App">
       <h1>React drag and drop component</h1>
+      <DragAndDrop />
     </div>
   );
 }
---
+++ src/DragAndDrop.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const DragAndDrop = props => {
+
+  const handleDragEnter = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragLeave = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDragOver = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  const handleDrop = e => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  return (
+    <div className={'drag-drop-zone'}
+      onDrop={e => handleDrop(e)}
+      onDragOver={e => handleDragOver(e)}
+      onDragEnter={e => handleDragEnter(e)}
+      onDragLeave={e => handleDragLeave(e)}
+    >
+      <p>Drag files here to upload</p>
+    </div>
+  );
+};
+
+export default DragAndDrop;
