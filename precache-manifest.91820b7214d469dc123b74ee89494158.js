e9eaeaa9b57f2ff7a3131d1f10208bf5c989bc09
Chidi Orji <orjichidi95@gmail.com>
2020-03-17 22:13:05 +0100

    Updates

---
+++ precache-manifest.91820b7214d469dc123b74ee89494158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b267092a753d65f9019d398fd6f01b75",
+    "url": "/react-dnd/index.html"
+  },
+  {
+    "revision": "477a5038dbf70d78545f",
+    "url": "/react-dnd/static/css/main.e0092dc2.chunk.css"
+  },
+  {
+    "revision": "955b49593e175fa65ae3",
+    "url": "/react-dnd/static/js/2.385f59e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-dnd/static/js/2.385f59e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "477a5038dbf70d78545f",
+    "url": "/react-dnd/static/js/main.b0720fb2.chunk.js"
+  },
+  {
+    "revision": "dbd62100ade7d788641c",
+    "url": "/react-dnd/static/js/runtime-main.cf45d206.js"
+  }
+]);
